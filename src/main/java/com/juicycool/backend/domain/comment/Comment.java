852123238a38a08496ecc33669d545c355e40e61
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-06-16 00:53:01 +0900

    add :: Comment 추가

---
+++ src/main/java/com/juicycool/backend/domain/comment/Comment.java
@@ -0,0 +1,31 @@
+package com.juicycool.backend.domain.comment;
+
+import com.juicycool.backend.domain.community.Community;
+import com.juicycool.backend.domain.user.User;
+import com.juicycool.backend.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "community_id")
+    private Community community;
+
+}
