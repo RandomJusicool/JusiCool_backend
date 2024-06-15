1fb9b204ee2d1ebf9ca74acf23fdec5417232fd4
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-06-16 00:17:42 +0900

    add :: Community 추가

---
+++ src/main/java/com/juicycool/backend/domain/community/Community.java
@@ -0,0 +1,29 @@
+package com.juicycool.backend.domain.community;
+
+import com.juicycool.backend.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class Community {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
