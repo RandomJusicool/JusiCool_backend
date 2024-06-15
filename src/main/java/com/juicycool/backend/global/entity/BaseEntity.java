c7d2c6898e201d05d1baa70251464f17974f4138
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-06-16 00:53:08 +0900

    add :: BaseEntity 추가

---
+++ src/main/java/com/juicycool/backend/global/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.juicycool.backend.global.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import lombok.Getter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    private LocalDateTime createAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createAt = ZonedDateTime.now(ZoneId.of("Asia/Seoul")).toLocalDateTime();
+    }
+}
