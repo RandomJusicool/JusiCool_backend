0f78860b3b41391272ed7696482592368386b9e2
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-06-17 00:38:27 +0900

    add :: url 추가

--- src/main/java/com/juicycool/backend/global/security/config/SecurityConfig.java
+++ src/main/java/com/juicycool/backend/global/security/config/SecurityConfig.java
@@ -7,10 +7,13 @@ import com.juicycool.backend.global.security.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsUtils;
 
@@ -43,6 +46,7 @@ public class SecurityConfig {
                                 .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
 
                                 // auth
+                                .requestMatchers(HttpMethod.POST, "/api/v1/auth/signup").permitAll()
 
                                 .anyRequest().denyAll()
 
