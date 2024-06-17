1c549183d25470d6f56400044e8539c5ab054a28
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-06-17 14:32:28 +0900

    add :: EmailAuthEntity 추가

---
+++ src/main/java/com/juicycool/backend/domain/email/EmailAuthEntity.java
@@ -0,0 +1,21 @@
+package com.juicycool.backend.domain.email;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+
+@RedisHash(value = "refreshToken", timeToLive = 60)
+@Builder
+@Getter
+public class EmailAuthEntity {
+
+    @Id
+    private String email;
+    @Indexed
+    private String randomValue;
+    private Boolean authentication;
+    private Integer attemptCount;
+
+}
