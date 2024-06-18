8257cf55bdce5260e86381cfa65743a11936c453
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-06-18 10:06:12 +0900

    update :: 이메일 인증 적용

--- src/main/java/com/juicycool/backend/domain/auth/service/impl/SignUpServiceImpl.java
+++ src/main/java/com/juicycool/backend/domain/auth/service/impl/SignUpServiceImpl.java
@@ -1,8 +1,11 @@
 package com.juicycool.backend.domain.auth.service.impl;
 
 import com.juicycool.backend.domain.auth.exception.DuplicateEmailException;
+import com.juicycool.backend.domain.auth.exception.NotVerificationMailException;
 import com.juicycool.backend.domain.auth.presentation.dto.request.SignUpRequestDto;
 import com.juicycool.backend.domain.auth.service.SignUpService;
+import com.juicycool.backend.domain.email.MailAuthEntity;
+import com.juicycool.backend.domain.email.repository.MailAuthRepository;
 import com.juicycool.backend.domain.user.User;
 import com.juicycool.backend.domain.user.repository.UserRepository;
 import com.juicycool.backend.global.annotation.TransactionService;
@@ -15,10 +18,19 @@ public class SignUpServiceImpl implements SignUpService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final MailAuthRepository mailAuthRepository;
 
     public void execute(SignUpRequestDto dto) {
-        if (userRepository.existsByEmail(dto.getEmail()))
+        if (userRepository.existsByEmail(dto.getEmail())) {
+            mailAuthRepository.deleteById(dto.getEmail());
             throw new DuplicateEmailException();
+        }
+
+        MailAuthEntity mailAuth = mailAuthRepository.findById(dto.getEmail())
+                .orElseThrow(NotVerificationMailException::new);
+
+        if (!mailAuth.getAuthentication())
+            throw new NotVerificationMailException();
 
         User user = User.builder()
                 .email(dto.getEmail())
@@ -26,6 +38,8 @@ public class SignUpServiceImpl implements SignUpService {
                 .password(passwordEncoder.encode(dto.getPassword()))
                 .points(100000)
                 .build();
+
+        mailAuthRepository.delete(mailAuth);
 
         userRepository.save(user);
 
