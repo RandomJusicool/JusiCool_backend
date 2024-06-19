25ae0ef9c11554c7e9a5506ab8968ec386da5ea3
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-06-19 23:13:37 +0900

    add :: JwtProperties 추가

---
+++ src/main/java/com/juicycool/backend/global/security/jwt/JwtProperties.java
@@ -0,0 +1,12 @@
+package com.juicycool.backend.global.security.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@AllArgsConstructor
+@ConfigurationProperties("jwt")
+public class JwtProperties {
+    private final String secret;
+}
