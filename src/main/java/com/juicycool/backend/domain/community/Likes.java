5d59111db24a93a48ac1ca569b9522078b34e64a
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-06-20 13:50:53 +0900

    update :: Likes 수정

--- src/main/java/com/juicycool/backend/domain/community/Likes.java
+++ src/main/java/com/juicycool/backend/domain/community/Likes.java
@@ -18,7 +18,7 @@ public class Likes {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "board_id")
     private Board board;
 }
