fd4524de17ad7241f14412bd4d28520f0c701782
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-06-20 11:24:46 +0900

    add :: Board 추가

---
+++ src/main/java/com/juicycool/backend/domain/community/Board.java
@@ -0,0 +1,37 @@
+package com.juicycool.backend.domain.community;
+
+import com.juicycool.backend.domain.user.User;
+import com.juicycool.backend.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Getter
+@Builder
+public class Board extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private Integer comment_num;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "community_id")
+    private Community community;
+
+
+}
