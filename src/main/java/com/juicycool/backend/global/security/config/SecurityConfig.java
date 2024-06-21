1a2509d8411bfe2be6781dbf749401de78147112
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-06-21 16:03:48 +0900

    add :: url 추가

--- src/main/java/com/juicycool/backend/global/security/config/SecurityConfig.java
+++ src/main/java/com/juicycool/backend/global/security/config/SecurityConfig.java
@@ -68,6 +68,9 @@ public class SecurityConfig {
                                 .requestMatchers(HttpMethod.PATCH, "/api/v1/board/{board_id}").authenticated()
                                 .requestMatchers(HttpMethod.DELETE, "/api/v1/board/{board_id}").authenticated()
 
+                                // reservation
+                                .requestMatchers(HttpMethod.GET, "/api/v1/reservation").authenticated()
+
                                 .anyRequest().denyAll()
 
 
