0eb62d7004feb45f9dda8182c1ddf63a8dde7c25
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-06-22 21:48:24 +0900

    update :: Stock 변수명 수정

--- src/main/java/com/juicycool/backend/domain/stock/Stock.java
+++ src/main/java/com/juicycool/backend/domain/stock/Stock.java
@@ -22,23 +22,23 @@ public class Stock {
 
     private Integer code; // 주식 코드
 
-    private Integer market_price; // 시가
+    private Integer marketPrice; // 시가
 
-    private Integer high_price; // 고가
+    private Integer highPrice; // 고가
 
-    private Integer low_price; // 저가
+    private Integer lowPrice; // 저가
 
-    private Integer present_price; // 현재가
+    private Integer presentPrice; // 현재가
 
-    private Integer contract_price; // 체결가
+    private Integer contractPrice; // 체결가
 
-    private Integer fluctuation_compared_previous_day; // 전일 대비 등락
+    private Integer fluctuationComparedPreviousDay; // 전일 대비 등락
 
-    private Long market_capitalization; // 시가 총액
+    private Long marketCapitalization; // 시가 총액
 
-    private Integer transaction_volume; // 거래량
+    private Integer transactionVolume; // 거래량
 
-    private Integer transaction_price; // 거래 대금
+    private Integer transactionPrice; // 거래 대금
 
 
 
