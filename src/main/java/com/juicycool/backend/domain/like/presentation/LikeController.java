4117438d4472e3f41d0fb65014029c384f3e1189
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-06-22 23:04:35 +0900

    add :: cancelLike api 추가

--- src/main/java/com/juicycool/backend/domain/like/presentation/LikeController.java
+++ src/main/java/com/juicycool/backend/domain/like/presentation/LikeController.java
@@ -1,13 +1,11 @@
 package com.juicycool.backend.domain.like.presentation;
 
 import com.juicycool.backend.domain.like.service.AddLikeService;
+import com.juicycool.backend.domain.like.service.CancelLikeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,10 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class LikeController {
 
     private final AddLikeService addLikeService;
+    private final CancelLikeService cancelLikeService;
 
     @PostMapping("/{board_id}")
     public ResponseEntity<Void> addLike(@PathVariable("board_id") Long boardId) {
         addLikeService.execute(boardId);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping("/{board_id}")
+    public ResponseEntity<Void> cancelLike(@PathVariable("board_id") Long boardId) {
+        cancelLikeService.execute(boardId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
