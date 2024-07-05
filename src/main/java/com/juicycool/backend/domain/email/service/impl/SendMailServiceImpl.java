8bdf2ada0bd546af7c34010e380ed16b87514516
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-07-05 14:49:10 +0900

    refactor :: sendEmail 리팩토링

--- src/main/java/com/juicycool/backend/domain/email/service/impl/SendMailServiceImpl.java
+++ src/main/java/com/juicycool/backend/domain/email/service/impl/SendMailServiceImpl.java
@@ -2,13 +2,18 @@ package com.juicycool.backend.domain.email.service.impl;
 
 import com.juicycool.backend.domain.email.MailAuthEntity;
 import com.juicycool.backend.domain.email.exception.AlreadyAuthenticatedMailException;
+import com.juicycool.backend.domain.email.exception.FailSendMailException;
 import com.juicycool.backend.domain.email.presentation.dto.request.SendMailRequestDto;
 import com.juicycool.backend.domain.email.repository.MailAuthRepository;
 import com.juicycool.backend.domain.email.service.SendMailService;
 import com.juicycool.backend.global.annotation.TransactionService;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
@@ -20,6 +25,8 @@ public class SendMailServiceImpl implements SendMailService {
 
     private final JavaMailSender mailSender;
     private final MailAuthRepository emailAuthRepository;
+    private final SpringTemplateEngine templateEngine;
+    public static final String EMAIL_SUBJECT = "JusiCool 이메일 인증";
 
     public void execute(SendMailRequestDto dto) {
         MailAuthEntity emailAuth = emailAuthRepository.findById(dto.getEmail())
@@ -34,7 +41,7 @@ public class SendMailServiceImpl implements SendMailService {
             throw new AlreadyAuthenticatedMailException();
 
         emailAuthRepository.save(emailAuth);
-        sendCode(dto.getEmail(), emailAuth.getRandomValue());
+        sendEmail(dto.getEmail(), emailAuth.getRandomValue());
     }
 
     private String createCode() {
@@ -51,13 +58,29 @@ public class SendMailServiceImpl implements SendMailService {
         return "";
     }
 
-    private void sendCode(String email, String code) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(email);
-        message.setSubject("[JusiCool] 이메일 인증");
-        message.setText(code);
+    public void sendEmail(String email, String authCode) {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper;
+        try {
+            helper = new MimeMessageHelper(message, "utf-8");
+            helper.setTo(email);
+            helper.setSubject(EMAIL_SUBJECT);
+            String context = setContext(authCode);
+            helper.setText(context, true);
 
-        mailSender.send(message);
+            mailSender.send(message);
+        } catch (MessagingException e) {
+            throw new FailSendMailException();
+        }
+    }
+
+    private String setContext(String authCode) {
+        String template = "certificationMailTemplate";
+        Context context = new Context();
+
+        context.setVariable("authenticationCode", authCode);
+
+        return templateEngine.process(template, context);
     }
 
 }
