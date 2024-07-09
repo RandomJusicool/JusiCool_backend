dee847ac43316ade3b32f293565960a828a90891
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-07-09 09:56:36 +0900

    add :: ExceptionFilter 추가

---
+++ src/main/java/com/juicycool/backend/global/filter/ExceptionFilter.java
@@ -0,0 +1,45 @@
+package com.juicycool.backend.global.filter;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.juicycool.backend.global.exception.ErrorCode;
+import com.juicycool.backend.global.exception.ErrorResponse;
+import com.juicycool.backend.global.exception.GlobalException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class ExceptionFilter extends OncePerRequestFilter {
+
+    private final ObjectMapper objectMapper;
+
+    public ExceptionFilter(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (GlobalException e) {
+            sendError(response, e.getErrorCode());
+        } catch (Exception e) {
+            sendError(response, ErrorCode.INTERNAL_SERVER_ERROR);
+            throw e;
+        }
+    }
+
+    private void sendError(HttpServletResponse response, ErrorCode errorCode) throws IOException {
+        ErrorResponse errorResponse = new ErrorResponse(errorCode.getStatus(), errorCode.getMessage());
+        String responseString = objectMapper.writeValueAsString(errorResponse);
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(errorCode.getStatus());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().write(responseString);
+    }
+}
