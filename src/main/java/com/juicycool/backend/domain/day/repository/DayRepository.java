37952e546b07701aa87efbf49780bd5a789bc607
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-07-10 17:55:43 +0900

    add :: findByCode 추가

--- src/main/java/com/juicycool/backend/domain/day/repository/DayRepository.java
+++ src/main/java/com/juicycool/backend/domain/day/repository/DayRepository.java
@@ -3,5 +3,8 @@ package com.juicycool.backend.domain.day.repository;
 import com.juicycool.backend.domain.day.Day;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DayRepository extends JpaRepository<Day, Long> {
+    List<Day> findByCode(Integer code);
 }
