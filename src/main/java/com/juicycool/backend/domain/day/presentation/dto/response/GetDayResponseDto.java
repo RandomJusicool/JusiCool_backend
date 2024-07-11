53b09d54b38e367bf0fc12fdbf165badce668815
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-07-11 17:21:35 +0900

    delete :: 종가 삭제

--- src/main/java/com/juicycool/backend/domain/day/presentation/dto/response/GetDayResponseDto.java
+++ src/main/java/com/juicycool/backend/domain/day/presentation/dto/response/GetDayResponseDto.java
@@ -10,7 +10,6 @@ public class GetDayResponseDto {
     private Long highPrice; // 고가
     private Long lowPrice; // 저가
     private Long presentPrice; // 현재가
-    private Long headPrice; // 종가
     private Long volume; // 거래량
     private Double upDownPercent; // 등략률
     private String storeAt; // 저장된 시간
