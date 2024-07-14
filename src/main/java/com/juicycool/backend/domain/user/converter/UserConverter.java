4fd1a3d076f477d765de0528a60c4f9c08648a4e
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-07-15 00:49:53 +0900

    add :: toPointDto 추가

--- src/main/java/com/juicycool/backend/domain/user/converter/UserConverter.java
+++ src/main/java/com/juicycool/backend/domain/user/converter/UserConverter.java
@@ -4,8 +4,10 @@ import com.juicycool.backend.domain.board.Board;
 import com.juicycool.backend.domain.stock.OwnedStocks;
 import com.juicycool.backend.domain.user.presentation.dto.response.GetMyBoardResponseDto;
 import com.juicycool.backend.domain.user.presentation.dto.response.GetMyOwnedStockResponseDto;
+import com.juicycool.backend.domain.user.presentation.dto.response.GetMyPointResponseDto;
 
 public interface UserConverter {
     GetMyOwnedStockResponseDto toDto(OwnedStocks ownedStock);
     GetMyBoardResponseDto toBoardDto(Board board);
+    GetMyPointResponseDto toPointDto(Long allPoint, Long price, Double percent);
 }
