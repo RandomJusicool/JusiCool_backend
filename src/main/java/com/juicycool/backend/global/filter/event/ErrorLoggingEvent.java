f8c7f3eddf1def8fa79be098fbf92b2d9c4c84b0
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-07-25 11:49:15 +0900

    delete :: HttpServletResponse 삭제

--- src/main/java/com/juicycool/backend/global/filter/event/ErrorLoggingEvent.java
+++ src/main/java/com/juicycool/backend/global/filter/event/ErrorLoggingEvent.java
@@ -1,13 +1,11 @@
 package com.juicycool.backend.global.filter.event;
 
 import com.juicycool.backend.global.exception.ErrorCode;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class ErrorLoggingEvent {
-    private HttpServletResponse response;
     private ErrorCode errorCode;
 }
