d98c5b9f14c4a653eca5215aca02263762d873f7
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-08-21 16:00:05 +0900

    add :: TokenParser DI 추가

--- src/main/java/com/juicycool/backend/global/security/config/JwtSecurityConfig.java
+++ src/main/java/com/juicycool/backend/global/security/config/JwtSecurityConfig.java
@@ -2,6 +2,7 @@ package com.juicycool.backend.global.security.config;
 
 import com.juicycool.backend.global.filter.JwtFilter;
 import com.juicycool.backend.global.security.jwt.JwtProvider;
+import com.juicycool.backend.global.security.jwt.TokenParser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,10 +12,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @RequiredArgsConstructor
 public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
     private final JwtProvider jwtProvider;
+    private final TokenParser tokenParser;
 
     @Override
     public void configure(HttpSecurity http) {
-        JwtFilter customFilter = new JwtFilter(jwtProvider);
+        JwtFilter customFilter = new JwtFilter(jwtProvider, tokenParser);
         http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
