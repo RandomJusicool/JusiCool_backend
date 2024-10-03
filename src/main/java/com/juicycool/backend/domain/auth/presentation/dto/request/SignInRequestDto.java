0e1f26ba979f8f774226ed29a7fe6f7be4fa64dc
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-10-04 01:00:24 +0900

    update :: NoArgs -> AllArgs

--- src/main/java/com/juicycool/backend/domain/auth/presentation/dto/request/SignInRequestDto.java
+++ src/main/java/com/juicycool/backend/domain/auth/presentation/dto/request/SignInRequestDto.java
@@ -1,11 +1,11 @@
 package com.juicycool.backend.domain.auth.presentation.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
+@AllArgsConstructor
 public class SignInRequestDto {
     @NotBlank
     private String email;
