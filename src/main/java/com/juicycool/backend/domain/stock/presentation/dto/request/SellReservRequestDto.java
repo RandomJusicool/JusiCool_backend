e6414edbf8d265f3eb01ef5bb7dd331d1402dc6d
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-10-05 20:27:05 +0900

    update :: NoArgs -> AllArgs 변경

--- src/main/java/com/juicycool/backend/domain/stock/presentation/dto/request/SellReservRequestDto.java
+++ src/main/java/com/juicycool/backend/domain/stock/presentation/dto/request/SellReservRequestDto.java
@@ -1,10 +1,10 @@
 package com.juicycool.backend.domain.stock.presentation.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
+@AllArgsConstructor
 public class SellReservRequestDto {
     private Long num;
     private Long goal_price;
